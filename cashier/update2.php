6a842fdcde8ffa15ecd11bbdbd4a9de83383adc1
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2022-05-25 15:32:46 +0200

    Create update2.php

---
+++ cashier/update2.php
@@ -0,0 +1,52 @@
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+</style>
+<?php
+ include "Menu.php";
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "lab07";
+ session_start();
+ 
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ $query = "SELECT * FROM user";
+ $result = mysqli_query($conn,$query);
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>  
+</head>
+<body>
+<table>
+<tr>
+<td>Id</td>
+<td>First Name</td>
+
+<td>Email id</td>
+<td>Age</td>
+</tr>
+<?php
+$i=0;
+while($row = mysqli_fetch_array($result)) {
+ 
+?>
+<tr class="<?php if(isset($classname)) echo $classname;?>">
+<td><?php echo $row["Id"]; ?></td>
+<td><?php echo $row["First Name"]; ?></td>
+<td><?php echo $row["Email"]; ?></td>
+<td><?php echo $row["Age"]; ?></td>
+<td><a href="UpdateAction.php?userid=<?php echo $row["Id"]; ?>">Update</a></td>
+</tr>
+<?php
+$i++;
+}
+?>
+</table>
+</body>
+</html>
