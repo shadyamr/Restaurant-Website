2c82c4a85c1ea7cacb17b22e881f6c2d88fa91b9
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2022-05-25 15:33:49 +0200

    Create View All.php

---
+++ cashier/View All.php
@@ -0,0 +1,61 @@
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+</style>
+<?php
+include "Menu.php";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lab07";
+session_start();
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$query = "SELECT * FROM user";
+$result = mysqli_query($conn,$query);
+ 
+
+?>
+<html>
+<form method="post" action="">
+<table class="table table-bordered">
+<thead>
+<tr>
+
+	<th> Id</th>
+	<th>First Name</th>
+	<th>Email</th>
+	<th>Age</th>
+  
+	 
+</tr>
+</thead>
+<?php
+ 
+while($row = mysqli_fetch_array($result)) 
+{
+    
+?>
+<tr>
+  
+	<td><?= $row['Id']; ?></td>
+	<td><?= $row['First Name']; ?></td>
+	<td><?=  $row['Email']; ?></td>
+	<td><?= $row['Age']; ?></td>
+  
+	 
+</tr>
+<?php
+ 
+}
+?>
+</table>
+ 
+</form>
+
+</body>
+</html>
+</html>
