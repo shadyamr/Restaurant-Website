07a0cdae1a313534a92b5ec1278863db395ede83
Ahmed Hossam <86417843+AhmedHossam9@users.noreply.github.com>
2022-06-09 15:33:17 +0200

    Update upload.php

--- archive/upload.php
+++ archive/upload.php
@@ -12,4 +12,21 @@ if (isset($_POST['submit'])){
     $fileActualExt = strtolower(end($fileExt)); //converts the file type to lower case letters.
     
     $allowed = array('jpeg','jpg','png','pdf');
-?>
+    if(in_array($fileActualExt, $allowed)){
+    if($fileError === 0) // check for file error uploads
+    {
+        if($fileSize < 1000000){
+        $fileNewName = uniqid('',true).".".$fileActualExt //Gives the file a unique ID so nothing gets replaced.
+        $fileDestination = 'file/'.$fileNewName;
+        move_uploaded_file($fileTmpName,$fileDestination); //Moves the file from its temporary location to our file destination.
+        } else {
+            echo "Your file is too big!";
+        }
+    } else{
+        echo "There was an error uploading this file, please try again!";
+    }
+    } else {
+        echo "ERROR! This type of file is not allowed!";
+    }
+}
+?>   
