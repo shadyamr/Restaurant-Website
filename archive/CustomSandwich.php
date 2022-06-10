62a6de4dc617dffa56c4cad3d41eadeb05be84c1
Ahmed Hossam <86417843+AhmedHossam9@users.noreply.github.com>
2022-06-10 22:10:20 +0200

    Update CustomSandwich.php

--- archive/CustomSandwich.php
+++ archive/CustomSandwich.php
@@ -1,23 +1,32 @@
-    <?php
+<?php
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "restaurant";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "restaurant";
 
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        $sql = "SELECT * FROM breadstock";
-        $result = mysqli_query($conn, $sql);
-    ?>
-        <select>
-        <option value="" disabled="" selected="">Select Bread!</option>
-        <?php  
-    while($bread = mysqli_fetch_array($result))
-    {
-    ?>
-                <option value="<?php echo $bread['BreadName']; ?>"><?php echo $bread['BreadName']." - ".$bread["BreadPrice"]; ?></option>
-                <?php
-            }?></select>
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM breadstock";
+$result = mysqli_query($conn, $sql);
+?>
+<form method="POST" action=>
+<select>
+<option value="" disabled="" selected="">Select Bread!</option>
+<?php  
+while($bread = mysqli_fetch_array($result))
+{
+?>
+        <option value="<?php echo $bread['BreadName']; ?>"><?php echo $bread['BreadName']." - ".$bread["BreadPrice"]; ?></option>
+        <?php
+    }?>  
+</select>
+<button name="submit" type="submit">Submit</button>
+<?php
+$testsubmit = $_POST['submit'];
+echo $testsubmit;
+$test2 = "INSERT INTO somposedsandwich(ChoosenBread) VALUES(".$_POST['submit'].")";
+?>
+</form>
 <br>
 <br>
 <?php
@@ -26,14 +35,14 @@ $result1 = mysqli_query($conn, $sql1);
 ?>
 
 <select>
-        <option value="" disabled="" selected="">Select Your First Component!</option>
-        <?php  
-    while($vegetable1 = mysqli_fetch_array($result1))
-    {
-    ?>
-                <option value="<?php echo $vegetable1['VegetableName']; ?>"><?php echo $vegetable1['VegetableName']." - ".$vegetable1["VegetablePrice"]; ?></option>
-                <?php
-            }?></select>
+<option value="" disabled="" selected="">Select Your First Component!</option>
+<?php  
+while($vegetable1 = mysqli_fetch_array($result1))
+{
+?>
+        <option value="<?php echo $vegetable1['VegetableName']; ?>"><?php echo $vegetable1['VegetableName']." - ".$vegetable1["VegetablePrice"]; ?></option>
+        <?php
+    }?></select>
 <br>
 <br>
 <?php
@@ -42,14 +51,14 @@ $result2 = mysqli_query($conn, $sql2);
 ?>
 
 <select>
-        <option value="" disabled="" selected="">Select Your Second Component!</option>
-        <?php  
-    while($vegetable2 = mysqli_fetch_array($result2))
-    {
-    ?>
-                <option value="<?php echo $vegetable2['VegetableName']; ?>"><?php echo $vegetable2['VegetableName']." - ".$vegetable2["VegetablePrice"]; ?></option>
-                <?php
-            }?></select>
+<option value="" disabled="" selected="">Select Your Second Component!</option>
+<?php  
+while($vegetable2 = mysqli_fetch_array($result2))
+{
+?>
+        <option value="<?php echo $vegetable2['VegetableName']; ?>"><?php echo $vegetable2['VegetableName']." - ".$vegetable2["VegetablePrice"]; ?></option>
+        <?php
+    }?></select>
 <br>
 <br>
 <?php
@@ -58,14 +67,14 @@ $result3 = mysqli_query($conn, $sql3);
 ?>
 
 <select>
-        <option value="" disabled="" selected="">Select Your Main Component!</option>
-        <?php  
-    while($main = mysqli_fetch_array($result3))
-    {
-    ?>
-                <option value="<?php echo $main['MainName']; ?>"><?php echo $main['MainName']." - ".$main["MainPrice"]; ?></option>
-                <?php
-            }?></select>
+<option value="" disabled="" selected="">Select Your Main Component!</option>
+<?php  
+while($main = mysqli_fetch_array($result3))
+{
+?>
+        <option value="<?php echo $main['MainName']; ?>"><?php echo $main['MainName']." - ".$main["MainPrice"]; ?></option>
+        <?php
+    }?></select>
 <br>
 <br>
 <?php
@@ -74,11 +83,30 @@ $result4 = mysqli_query($conn, $sql4);
 ?>
 
 <select>
-        <option value="" disabled="" selected="">Select Your Side Sauce!</option>
-        <?php  
-    while($sauce = mysqli_fetch_array($result4))
+<option value="" disabled="" selected="">Select Your Side Sauce!</option>
+<?php  
+while($sauce = mysqli_fetch_array($result4))
+{
+?>
+        <option value="<?php echo $sauce['SauceName']; ?>"><?php echo $sauce['SauceName']." - ".$sauce["SaucePrice"]; ?></option>
+        <?php
+    }?></select>
+
+<?php
+if(isset($_POST['BreadName']))
+{
+    $bread = $_POST['BreadName'];
+
+    $query = "INSERT INTO `somposesandwich`(`ChoosenBread`,`ChoosenComponent1`,`ChoosenComponent2`,`ChoosenMain`,`ChoosenSauce`) VALUES ('$bread','$vegetable1','$vegetable2','$main','$sauce')";
+    $query_run = mysqli_query($conn,$query);
+
+    if($result5)
     {
-    ?>
-                <option value="<?php echo $sauce['SauceName']; ?>"><?php echo $sauce['SauceName']." - ".$sauce["SaucePrice"]; ?></option>
-                <?php
-            }?></select>
+        echo '<script type = "text/javascript"> alert("Data Saved") </script> ';
+    }
+    else
+    {
+        echo '<script type = "text/javascript"> alert("Data Not Saved") </script> ';
+    }
+}
+?>
